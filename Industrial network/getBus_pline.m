8116cd6519bd08656fe29cce17f50db7013baf02
Yuqi-Wang <54396508+Yuqi-Wang@users.noreply.github.com>
2024-03-12 20:16:15 +0800

    Delete Industrial network directory

--- Industrial network/getBus_pline.m
+++
@@ -1,3 +0,0 @@
-function bus = getBus_pline(cnum,j,t,n)
-% 计算图片中节点编号,根据线路潮流或电流变量的编号
-bus = (cnum-24*j-(t+1)-23-24*(n-1))/(24*n)+1 + 2;
--- Industrial network/getNum.m
+++
@@ -1,5 +0,0 @@
-function [line,s,t] = getNum(N)
-N = N + 1;
-line = floor(N/2400)+1;
-s = floor((N-(line-1)*2400)/24)+1;
-t = N - (line-1)*2400 - (s-1)*24;
